bca26a8758760ac6cb1e30b5a069713f33c64623
Chris Withers <chris@withers.org>
2019-05-22 19:59:29 +0100

    pydantic has this already :-)

--- backend/diary/config.py
+++ backend/diary/config.py
@@ -1,32 +1,9 @@
 import os
-from importlib import import_module
 from pathlib import Path
 from typing import List
 
 from configurator import Config
-from pydantic import BaseModel, PydanticValueError, urlstr
-from pydantic.validators import str_validator
-
-
-class ImportValueError(PydanticValueError):
-    code = 'import'
-    msg_template = 'Could not import {path}: {message}'
-
-
-class Import(str):
-    @classmethod
-    def __get_validators__(cls):
-        yield str_validator
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, v):
-        module_name, obj_name = v.split(':')
-        try:
-            module =  import_module(module_name)
-            return getattr(module, obj_name)
-        except (ImportError, AttributeError) as e:
-            raise ImportValueError(message=str(e), path=v)
+from pydantic import BaseModel, PyObject, urlstr
 
 
 class DatabaseConfig(BaseModel):
@@ -35,13 +12,13 @@ class DatabaseConfig(BaseModel):
 
 class AppConfig(BaseModel):
     db: DatabaseConfig = ...
-    middleware: List[Import] = []
+    middleware: List[PyObject] = []
 
 
 def load_config():
     # defaults
     config = Config({
-        'middleware': ['diary.api:make_session'],
+        'middleware': ['diary.api.make_db_session'],
     })
 
     # file
