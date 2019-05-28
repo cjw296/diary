01315505cb8c7dcbd018343875fa057c64f2b9e6
Chris Withers <chris@withers.org>
2019-05-28 07:43:34 +0100

    Simplify test setup.
    Thanks to Mike for pointing me at:
    https://docs.sqlalchemy.org/en/13/orm/session_transaction.html#joining-a-session-into-an-external-transaction-such-as-for-test-suites
    
    - session.flush() is needed as there's one transaction but two sessions in play.
    
    - begin_nested() is so that even when a url handler commits, it doesn't pollute the database.
    
    - the bashing of DB_URL into os.environ could be cleaner, but needs support from Configurators

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -1,47 +1,29 @@
 import os
-from contextlib import contextmanager
 
 import pytest
+from diary.api import app
 from diary.model import Session, Base, Event, Done
 from sqlalchemy import create_engine
-from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.requests import Request
 from starlette.testclient import TestClient
-from diary.api import app, db_session_middleware
-
-client = TestClient(app)
 
 
-@contextmanager
-def replace_middleware_dispatch(app, existing, replacement):
-    app = app.error_middleware
-    while True:
-        app = getattr(app, 'app', None)
-        if app is None:
-            break
-        if isinstance(app, BaseHTTPMiddleware) and app.dispatch_func is existing:
-            app.dispatch_func = replacement
-    yield
-    # This should put things back like they were ;-)
+@pytest.fixture(scope='session')
+def client():
+    os.environ['DB_URL'] = os.environ['TEST_DB_URL']
+    with TestClient(app) as client:
+        yield client
 
 
-@pytest.fixture(scope='session', autouse=True)
-def db():
+@pytest.fixture(scope='session')
+def db(client):
     engine = create_engine(os.environ['TEST_DB_URL'])
     conn = engine.connect()
     transaction = conn.begin()
     try:
-        session = Session(bind=conn)
+        Session.configure(bind=conn)
+        session = Session()
         Base.metadata.create_all(bind=conn, checkfirst=False)
-
-        async def testing_session(request: Request, call_next):
-            request.state.db = session
-            return await call_next(request)
-
-        with replace_middleware_dispatch(
-            app, db_session_middleware, testing_session
-        ):
-            yield session
+        yield session
     finally:
         transaction.rollback()
 
@@ -50,22 +32,23 @@ def db():
 def session(db):
     transaction = db.begin_nested()
     try:
-            yield db
+        yield db
     finally:
         transaction.rollback()
 
 
-def test_root_empty(session):
+def test_root_empty(session, client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {'count': 0}
 
 
-def test_root_entries(session):
+def test_root_entries(session, client):
     session.add_all((
         Event(text='test'),
         Done(text='something'),
     ))
+    session.flush()
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {'count': 2}
