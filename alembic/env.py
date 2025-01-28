21c75ec0ffd2a33afe992fc88d0084bd59958fe4
Chris Withers <chris@simplistix.co.uk>
2025-01-28 08:23:12 +0000

    Unused import

--- alembic/env.py
+++ alembic/env.py
@@ -1,7 +1,7 @@
 import os
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config, create_engine
+from sqlalchemy import create_engine
 from sqlalchemy import pool
 
 from alembic import context
