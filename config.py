67b08b0b86a5945419fa57424d9bc7dd372a2988
Chris Withers <chris@simplistix.co.uk>
2025-01-29 07:55:56 +0000

    Minimal user management commands

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ SECRET_KEY: str = secrets.token_urlsafe(32)
 # 60 minutes * 24 hours * 8 days = 8 days
 ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
 
-def read_config() -> Config:
-    config = Config.from_path('config.yaml')
+def read_config(path: str = 'config.yaml') -> Config:
+    config = Config.from_path(path)
     config.diary_path = Path(config.diary_path).expanduser()
     if config.get('zope'):
         config.zope = Client(**config.zope.data)
---
+++ manage.py
@@ -0,0 +1,55 @@
+from contextlib import contextmanager
+from dataclasses import dataclass
+
+import click
+from sqlalchemy import create_engine, Engine, delete
+
+from config import read_config
+from models.user import User, UserCreate
+from sqlmodel import Session
+
+
+@dataclass
+class Database:
+    engine: Engine
+    session: Session
+
+@contextmanager
+def database(config_path: str):
+    engine = create_engine(read_config(config_path).db)
+    with Session(engine) as session:
+        yield Database(engine, session)
+
+
+@click.group()
+@click.option("--config", "config_path", type=click.Path(exists=True), default="config.yaml")
+@click.pass_context
+def cli(ctx: click.Context, config_path: str) -> None:
+    ctx.obj = ctx.with_resource(database(config_path))
+
+
+@cli.group()
+def user():
+    """User management"""
+
+
+@user.command('add')
+@click.pass_context
+@click.argument('email')
+@click.option('--password', prompt=True, hide_input=True)
+@click.option('--superuser', 'superuser', is_flag=True, default=False)
+def user_add(ctx: click.Context, email: str, password: str, superuser: bool) -> None:
+    User.create(ctx.obj.session, UserCreate(email=email, is_superuser=superuser, password=password))
+    ctx.obj.session.commit()
+
+
+@user.command('delete')
+@click.pass_context
+@click.argument('email')
+def user_delete(ctx: click.Context, email) -> None:
+    ctx.obj.session.exec(delete(User).where(User.email == email))
+    ctx.obj.session.commit()
+
+
+if __name__ == '__main__':
+    cli()
