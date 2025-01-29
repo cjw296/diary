75c05f5a3f5286fc1c01792f314033cf423e85fc
Chris Withers <chris@simplistix.co.uk>
2025-01-29 07:54:43 +0000

    Remove /items 404

--- frontend/src/components/Common/SidebarItems.tsx
+++ frontend/src/components/Common/SidebarItems.tsx
@@ -7,7 +7,6 @@ import type { UserPublic } from "../../client"
 
 const items = [
   { icon: FiHome, title: "Dashboard", path: "/" },
-  { icon: FiBriefcase, title: "Items", path: "/items" },
   { icon: FiSettings, title: "User Settings", path: "/settings" },
 ]
 
